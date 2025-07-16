635dacf1f4d74373463227cb695685053fb6ab96
kali <paul.maceres00@gmail.com>
2025-07-16 13:01:04 +0800

    initial commit

---
+++ sanctum/src/main/java/com/kali/sanctum/SanctumApplication.java
@@ -0,0 +1,13 @@
+package com.kali.sanctum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanctumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanctumApplication.class, args);
+	}
+
+}
---
+++ sanctum/src/test/java/com/kali/sanctum/SanctumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kali.sanctum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanctumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
