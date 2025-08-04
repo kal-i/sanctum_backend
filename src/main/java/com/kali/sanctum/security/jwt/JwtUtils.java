1b4f735ba564d69f9222f705e730aa62c4299217
kali <paul.maceres00@gmail.com>
2025-08-05 01:46:44 +0800

    feat(jwt): implement JwtUtils for token gen, validation, and extracting user info

---
+++ src/main/java/com/kali/sanctum/security/jwt/JwtUtils.java
@@ -0,0 +1,69 @@
+package com.kali.sanctum.security.jwt;
+
+import com.kali.sanctum.security.user.CustomUserDetails;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtUtils {
+    @Value("${auth.token.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${auth.token.expirationInMils}")
+    private int expirationTime;
+
+    public String generateTokenForUser(Authentication authentication) {
+        CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return Jwts.builder()
+                .subject(userPrincipal.getEmail())
+                .claim("id", userPrincipal.getId())
+                .claim("roles", roles)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + expirationTime))
+                .signWith(key(), Jwts.SIG.HS256)
+                .compact();
+    }
+
+    private SecretKey key() {
+        return Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret));
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key())
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+        } catch (ExpiredJwtException | UnsupportedJwtException e) {
+            throw new JwtException(e.getMessage());
+        }
+    }
+}
