76383b5744b593cc5c780661c6c926354a93bba4
kali <paul.maceres00@gmail.com>
2025-09-09 09:37:19 +0800

    Refactor JwtResponse entity to use UserDto, accessToken, and refreshToken instead id and token

--- src/main/java/com/kali/sanctum/dto/response/JwtResponse.java
+++ src/main/java/com/kali/sanctum/dto/response/JwtResponse.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JwtResponse {
-    private Long id;
-    private String token;
+    private UserDto userDto;
+    private String accessToken;
+    private String refreshToken;
 }
