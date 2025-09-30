c009f594117e32caa4892f1622ecb9ffb4455288
kali <paul.maceres00@gmail.com>
2025-10-01 02:39:39 +0800

    chore: Remove unncessary import

--- src/main/java/com/kali/sanctum/repository/SessionRepository.java
+++ src/main/java/com/kali/sanctum/repository/SessionRepository.java
@@ -3,7 +3,6 @@ package com.kali.sanctum.repository;
 import com.kali.sanctum.enums.TokenStatus;
 import com.kali.sanctum.model.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
