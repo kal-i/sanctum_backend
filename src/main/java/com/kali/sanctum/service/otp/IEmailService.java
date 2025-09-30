ff7fa3e703f34bfad3481deced53aed403f630a1
kali <paul.maceres00@gmail.com>
2025-10-01 02:43:37 +0800

    chore: Remove unnecessary import

--- src/main/java/com/kali/sanctum/service/otp/IEmailService.java
+++ src/main/java/com/kali/sanctum/service/otp/IEmailService.java
@@ -2,7 +2,6 @@ package com.kali.sanctum.service.otp;
 
 import com.kali.sanctum.dto.request.SendOtpRequest;
 import com.kali.sanctum.dto.request.VerifyOtpRequest;
-import com.kali.sanctum.enums.OtpPurpose;
 
 public interface IEmailService {
     void sendOtp(SendOtpRequest request);
