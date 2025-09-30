001d3caca28017d98d84563cc96f4db10d34e5e9
kali <paul.maceres00@gmail.com>
2025-10-01 02:37:44 +0800

    chore: Remove unnecessary import

--- src/main/java/com/kali/sanctum/dto/request/VerifyOtpRequest.java
+++ src/main/java/com/kali/sanctum/dto/request/VerifyOtpRequest.java
@@ -3,7 +3,6 @@ package com.kali.sanctum.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
-import lombok.Data;
 
 @Builder
 public record VerifyOtpRequest(
