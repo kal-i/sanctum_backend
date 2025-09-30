bbf512eb6b7ccb4a1650e2b61a7281bf054b990e
kali <paul.maceres00@gmail.com>
2025-10-01 02:42:44 +0800

    chore: Remove unnecessary import

--- src/main/java/com/kali/sanctum/service/auth/IAuthService.java
+++ src/main/java/com/kali/sanctum/service/auth/IAuthService.java
@@ -5,7 +5,6 @@ import com.kali.sanctum.dto.request.LoginRequest;
 import com.kali.sanctum.dto.request.RefreshTokenRequest;
 import com.kali.sanctum.dto.response.JwtResponse;
 import com.kali.sanctum.dto.response.UserDto;
-import com.kali.sanctum.model.User;
 
 public interface IAuthService {
     JwtResponse login(LoginRequest request);
