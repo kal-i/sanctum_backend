5c904b76f5ca5e7cfaac18e6f56153c164263f56
kali <paul.maceres00@gmail.com>
2025-10-03 15:57:27 +0800

    feat(dto): Add MoodDto

---
+++ src/main/java/com/kali/sanctum/dto/response/MoodDto.java
@@ -0,0 +1,12 @@
+package com.kali.sanctum.dto.response;
+
+import lombok.Data;
+
+@Data
+public class MoodDto {
+        private Long id;
+        private String name;
+        private int color;
+        private String icon;
+
+}
