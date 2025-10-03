c4fff2b9cf1cea4af057874956474f56f3200366
kali <paul.maceres00@gmail.com>
2025-10-03 14:37:38 +0800

    feat(daily_mood_check_repository): Add new custom method query existsByUserAndTimestampCreatedAtBetween()

--- src/main/java/com/kali/sanctum/repository/DailyMoodCheckRepository.java
+++ src/main/java/com/kali/sanctum/repository/DailyMoodCheckRepository.java
@@ -2,10 +2,14 @@ package com.kali.sanctum.repository;
 
 import com.kali.sanctum.model.DailyMoodCheck;
 import com.kali.sanctum.model.User;
+
+import java.time.Instant;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DailyMoodCheckRepository extends JpaRepository<DailyMoodCheck, Long> {
     Page<DailyMoodCheck> findByUser(User user, Pageable pageable);
+    boolean existsByUserAndTimestampCreatedAtBetween(User user, Instant start, Instant end);
 }
