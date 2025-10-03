4dabf4954e0e6d8a0f926629551e4f8e78fa3a67
kali <paul.maceres00@gmail.com>
2025-10-03 15:55:23 +0800

    feat(dto): Add LogDailyMoodCheckRequest DTO

---
+++ src/main/java/com/kali/sanctum/dto/request/LogDailyMoodCheckRequest.java
@@ -0,0 +1,18 @@
+package com.kali.sanctum.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+
+import java.util.Set;
+
+@Builder
+public record LogDailyMoodCheckRequest(
+        @NotNull
+        Long moodId,
+        @NotNull
+        Long reflectionPromptId,
+        Set<String> threeWordSummary,
+        @NotBlank
+        String entry
+) {}
