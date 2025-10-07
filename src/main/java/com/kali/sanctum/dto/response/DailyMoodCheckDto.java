34013fefbc91d3713f4d3f5071fa0e9e3ad1ad9e
kali <paul.maceres00@gmail.com>
2025-10-07 21:22:31 +0800

    refactor(dto): Remove MoodDto relation from DailyMoodCheckDto

--- src/main/java/com/kali/sanctum/dto/response/DailyMoodCheckDto.java
+++ src/main/java/com/kali/sanctum/dto/response/DailyMoodCheckDto.java
@@ -7,7 +7,6 @@ import lombok.Data;
 @Data
 public class DailyMoodCheckDto {
         private Long id;
-        private MoodDto moodDto;
         private Set<String> threeWordSummary;
         private ReflectionDto reflectionDto;
         private TimestampDto timestampDto;
