95c1246328316103933de31fcde258c81124baf5
kali <paul.maceres00@gmail.com>
2025-10-08 01:23:35 +0800

    feat(config): Add OpenAIConfig class and OpenAIClient Bean for dependency injection

---
+++ src/main/java/com/kali/sanctum/config/OpenAIConfig.java
@@ -0,0 +1,23 @@
+package com.kali.sanctum.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+
+
+@Configuration
+public class OpenAIConfig {
+
+    @Value("${openai.api-key}")
+    private String apiKey;
+
+    @Bean
+    public OpenAIClient openAIClient() {
+        return OpenAIOkHttpClient.builder()
+        .apiKey(apiKey)
+        .build();
+    }
+}
