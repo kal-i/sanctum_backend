fc837f23cff660da0c17eadc833db18b2abed1ce
kali <paul.maceres00@gmail.com>
2025-10-14 03:16:30 +0800

    refactor(dto): convert record to standard DTO class

--- src/main/java/com/kali/sanctum/dto/response/UserDto.java
+++ src/main/java/com/kali/sanctum/dto/response/UserDto.java
@@ -2,18 +2,25 @@ package com.kali.sanctum.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kali.sanctum.enums.Role;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Builder
-public record UserDto(
-        Long id,
-        String email,
-        String username,
-        String profileImageUrl,
-        Role role,
-        Set<String> permissions,
-        @JsonProperty("isVerified")
-        boolean isVerified
-) {}
+public class UserDto {
+        private Long id;
+        private String email;
+        private String username;
+        private String profileImageUrl;
+        private Role role;
+        private Set<String> permissions;
+        @JsonProperty("verified")
+        private boolean isVerified;
+}
