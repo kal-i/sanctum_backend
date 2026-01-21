bb1a5078b015374fa521e19aef01568b9cb5b291
kali <paul.maceres00@gmail.com>
2026-01-22 00:18:58 +0800

    refactor(service): change return type of getAllMoods(), getMoodById(), createdMood(), and updateMood()  methods from entity-to-DTO and add three new methods: getMoodEntityById(), ensureMoodDoestNotExist(), and existsByName()

--- src/main/java/com/kali/sanctum/service/mood/IMoodService.java
+++ src/main/java/com/kali/sanctum/service/mood/IMoodService.java
@@ -3,12 +3,18 @@ package com.kali.sanctum.service.mood;
 import java.util.List;
 
 import com.kali.sanctum.dto.request.CreateMoodRequest;
+import com.kali.sanctum.dto.request.UpdateMoodRequest;
 import com.kali.sanctum.dto.response.MoodDto;
 import com.kali.sanctum.model.Mood;
 
 public interface IMoodService {
-    List<Mood> getAllMoods();
-    Mood getMoodById(Long id);
-    Mood createMood(CreateMoodRequest request);
+    List<MoodDto> getAllMoods();
+    MoodDto getMoodById(Long id);
+    MoodDto createMood(CreateMoodRequest request);
+    MoodDto updateMood(Long id, UpdateMoodRequest request);
+    void deleteMood(Long id);
     MoodDto convertToDto(Mood mood);
+    Mood getMoodEntityById(Long id);
+    void ensureMoodDoesNotExist(String name);
+    boolean existsByName(String name);
 }
--- src/main/java/com/kali/sanctum/service/mood/MoodService.java
+++ src/main/java/com/kali/sanctum/service/mood/MoodService.java
@@ -1,11 +1,17 @@
 package com.kali.sanctum.service.mood;
 
 import com.kali.sanctum.dto.request.CreateMoodRequest;
+import com.kali.sanctum.dto.request.UpdateMoodRequest;
 import com.kali.sanctum.dto.response.MoodDto;
+import com.kali.sanctum.enums.AuditLogType;
 import com.kali.sanctum.exceptions.AlreadyExistsException;
 import com.kali.sanctum.exceptions.ResourceNotFoundException;
 import com.kali.sanctum.model.Mood;
+import com.kali.sanctum.model.User;
 import com.kali.sanctum.repository.MoodRepository;
+import com.kali.sanctum.service.audit.IAuditLogService;
+import com.kali.sanctum.service.user.IUserService;
+
 import lombok.RequiredArgsConstructor;
 
 import org.modelmapper.ModelMapper;
@@ -18,36 +24,118 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MoodService implements IMoodService {
     private final MoodRepository moodRepository;
+    private final IUserService userService;
+    private final IAuditLogService auditLogService;
     private final ModelMapper modelMapper;
 
-        @Override
-    public List<Mood> getAllMoods() {
-        return moodRepository.findAll();
+    @Override
+    public List<MoodDto> getAllMoods() {
+        List<Mood> moods = moodRepository.findAll();
+        List<MoodDto> moodDtos = moods.stream()
+                .map(this::convertToDto).toList();
+        return moodDtos;
     }
 
     @Override
-    public Mood getMoodById(Long id) {
+    public MoodDto getMoodById(Long id) {
+        Mood mood = getMoodEntityById(id);
+        return convertToDto(mood);
+    }
+
+    @Override
+    public Mood getMoodEntityById(Long id) {
         return moodRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Mood not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Mood not found."));
     }
 
     @Override
-    public Mood createMood(CreateMoodRequest request) {
+    public MoodDto createMood(CreateMoodRequest request) {
         return Optional.of(request)
                 .filter(mood -> !moodRepository.existsByName(mood.name()))
                 .map(req -> {
                     Mood mood = Mood.builder()
                             .name(req.name())
-                            .color(req.color())
+                            .color(req.color().intValue())
                             .icon(req.icon())
                             .build();
 
-                    return moodRepository.save(mood);
-                }).orElseThrow(() -> new AlreadyExistsException(request.name() + " already exists"));
+                    Mood savedMood = moodRepository.save(mood);
+
+                    User actor = userService.getAuthenticatedUser();
+
+                    auditLogService.logAction(
+                            actor.getId(),
+                            AuditLogType.CREATE_MOOD,
+                            savedMood.getId(),
+                            "Created mood " + savedMood.getName());
+
+                    return convertToDto(savedMood);
+                }).orElseThrow(() -> new AlreadyExistsException(request.name() + " already exists."));
+    }
+
+    @Override
+    public MoodDto updateMood(Long id, UpdateMoodRequest request) {
+        return moodRepository.findById(id)
+                .map(existingMood -> {
+                    // We're doingn service-layer validation because our DTO fields are optional
+                    if (request.name() != null && !request.name().isBlank()) {
+                        ensureMoodDoesNotExist(request.name());
+                        existingMood.setName(request.name());
+                    }
+
+                    if (request.color() != null) {
+                        existingMood.setColor(request.color());
+                    }
+
+                    if (request.icon() != null && !request.name().isBlank()) {
+                        existingMood.setIcon(request.icon());
+                    }
+
+                    Mood updatedMood = moodRepository.save(existingMood);
+
+                    User actor = userService.getAuthenticatedUser();
+
+                    auditLogService.logAction(
+                            actor.getId(),
+                            AuditLogType.UPDATE_MOOD,
+                            updatedMood.getId(),
+                            "Updated mood with IDs: " + updatedMood.getId());
+
+                    return convertToDto(updatedMood);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Mood not found."));
+    }
+
+    @Override
+    public void deleteMood(Long id) {
+        moodRepository.findById(id)
+                .ifPresentOrElse(mood -> {
+                    moodRepository.delete(mood);
+
+                    User actor = userService.getAuthenticatedUser();
+
+                    auditLogService.logAction(
+                            actor.getId(),
+                            AuditLogType.UPDATE_MOOD,
+                            mood.getId(),
+                            "Removed mood with IDs: " + mood.getId());
+                }, () -> new ResourceNotFoundException("Mood not found."));
     }
 
     @Override
     public MoodDto convertToDto(Mood mood) {
         return modelMapper.map(mood, MoodDto.class);
+    }
+
+    @Override
+    public void ensureMoodDoesNotExist(String name) {
+        if (existsByName(name)) {
+            throw new AlreadyExistsException("Mood " + name + " already exists.");
+        }
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return moodRepository.existsByName(name);
     }
 }
