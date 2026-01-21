19ddae9053e6429c4907c8c6e27779a7a60da366
kali <paul.maceres00@gmail.com>
2026-01-21 23:37:47 +0800

    feat(dto): add custom validation messages and nullable color component

--- src/main/java/com/kali/sanctum/dto/request/CreateMoodRequest.java
+++ src/main/java/com/kali/sanctum/dto/request/CreateMoodRequest.java
@@ -1,12 +1,16 @@
 package com.kali.sanctum.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record CreateMoodRequest(
-        @NotBlank
+        @NotBlank(message = "Mood name is required.")
         String name,
-        int color,
+
+        @NotNull(message = "Mood color is required.")
+        Integer color,
+
         String icon
 ) {}
